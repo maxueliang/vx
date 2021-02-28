960cf126ba1a1070a4aea40cd83f71bfeb1cdf3d
maxueliang <644766713@qq.com>
2021-02-28 18:46:43 +0800

    第一次提交

---
+++ pages/index/index.js
@@ -0,0 +1,118 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    navList:[],
+    currentIndexNAV:0,
+    swiperList:[],
+    videoList:[],
+  },
+  avtiveNAV(e){
+    // console.log(123);
+    this.setData({
+      currentIndexNAV:e.target.dataset.index
+    })
+  },
+
+  getNavList(){
+    let that=this;
+    wx.request({
+      url: 'http://mock-api.com/mnEe4VnJ.mock/navList',
+      success(res){
+        if (res.data.code===0) {
+          that.setData({
+            navList:res.data.data.navList
+          })
+        }
+      }
+    })
+  },
+  getSwiperList(){
+    let that=this
+    wx.request({
+      url: 'http://mock-api.com/mnEe4VnJ.mock/swiperList',
+      success(res){
+        if (res.data.code===0) {
+          that.setData({
+            swiperList:res.data.data.swiperList
+          })
+        }
+      }
+    })
+
+  },
+  getVideoList(){
+    let that=this
+    wx.request({
+      url: 'http://mock-api.com/mnEe4VnJ.mock/videoList',
+      success(res){
+        if (res.data.code===0) {
+          that.setData({
+            videosList:res.data.data.videoList
+          })
+        }
+      }
+    })
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.getNavList();
+    this.getSwiperList();
+    this.getVideoList();
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
